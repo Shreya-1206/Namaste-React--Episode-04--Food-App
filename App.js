1206927eb7229c5118de9cdd52b41a7f964fe420
Shreya <shreyaray.1206@gmail.com>
2024-04-02 18:10:15 +0530

    episode -04 Food-App

--- App.js
+++ App.js
@@ -20,11 +20,44 @@ const Header = () => {
     )
 }
 
+const RestaurantCard =() => {
+    return (
+        <div className="res-card">
+            <img alt = "res-img"src="https://b.zmtcdn.com/data/pictures/chains/1/50691/92d9b4053ef0965120828b4fa4eecc3b_o2_featured_v2.jpg"/>
+            <h3>Meghana Foods</h3>
+            <h5>South Indian, Briyani</h5>
+            <h5>4.4 stars</h5>
+            <h5>38 minutes</h5>
+        </div>
+    )
+}
+const Body =() => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    );
+}
 const AppLayout =() => { //main component
     return (
         // <h1>Hello Welcome to app 🍕</h1>
         <div className="app">
          <Header/>
+         <Body/>
         </div>
     );
 }
